3bfb51801785935428fef978c4099e3b643dc2df
Elirv <elizabeth.ramos101@hotmail.com>
2022-09-01 18:15:35 +0100

    ejercicios

--- 04.operators.php
+++ 04.operators.php
@@ -20,7 +20,16 @@ var_dump(5>3);
 echo"<br>";
 var_dump(5<3);
 echo"<br>";
-var_dump("3"==3);
+var_dump(5>=3);
 echo"<br>";
+var_dump(2<=3);
+echo"<br>";
+
+var_dump(2 &&4 <=3);
+echo"<br>";
+var_dump(0||5>1);
+echo"<br>";
+// var_dump(2!3);
+// echo"<br>";
 
 ?>
--- 05.dates.php
+++ 05.dates.php
@@ -1,4 +1,11 @@
 <?php
+  $a = array ('a' => 10, 'b' => 12, 'c' => 13);
+  $keys = array_keys($a);
 
+  shuffle($a);
+  var_dump($a); //Devolverá un array con los índices y valores mezclados de forma aleatoria
 
+  foreach ($keys as $key) {
+    echo $key . " - " . $a[$key]; //Devolverá una lista con valores mezclados de forma aleatoria y sus índices originales 
+  }
 ?>
