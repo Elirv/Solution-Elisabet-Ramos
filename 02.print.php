8c3a968367fc6c68fa714f71a4d9987d390606ed
Elirv <elizabeth.ramos101@hotmail.com>
2022-09-01 14:02:51 +0100

    hasta iterators

---
+++ 02.print.php
@@ -0,0 +1,10 @@
+<?php
+
+echo "<p>Mi primer código</p>", "<h3>puedo hacer varios</h3>";
+
+print "<h1>Solo puedo poner uno</h1>";
+
+$array = array ("diez"=>10, "nombre"=>"Eli", "edad"=>35,);
+print_r ($array);
+
+?>
---
+++ 03.iterators.php
@@ -0,0 +1,11 @@
+<?php
+
+
+$num=0;
+do{
+    
+    $num=$num+1;
+}
+while($num<=5);
+echo $num;
+?>
