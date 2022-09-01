f176263efe29d856a5c2b0c28b4eebd14f8a3460
Elirv <elizabeth.ramos101@hotmail.com>
2022-09-01 14:32:59 +0100

    hasta operatorss

--- 03.iterators.php
+++ 03.iterators.php
@@ -1,6 +1,26 @@
 <?php
 
+#for
+$i = 0;
+for ($i=0; $i <= 5; $i++){
+echo $i;
+}
+echo"<br>";
+#forEach
+$frutas = ['peras', 'manzanas', 'uvas'];
+foreach ($frutas as $propiedad => $valor){
+echo $propiedad ." ==> ". $valor."  ";
+}
 
+echo"<br>";
+#while
+$i = 0;
+while ($i <= 5){
+    echo $i;
+    $i++;
+}
+echo"<br>";
+#do...while
 $num=0;
 do{
     
@@ -8,4 +28,5 @@ do{
 }
 while($num<=5);
 echo $num;
+
 ?>
