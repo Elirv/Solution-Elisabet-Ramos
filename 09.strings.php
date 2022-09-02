ae95cc09df1c5ca7ba38ca390d77605e9886c14c
Elirv <elizabeth.ramos101@hotmail.com>
2022-09-02 19:13:14 +0100

    falta una string

--- 09.strings.php
+++ 09.strings.php
@@ -13,39 +13,68 @@ echo "<br>";
 echo "Hello my name is " . $name;
 echo "<br>";
 
-#string case sensitive
+#función reemplazar texto con mayus/min
+function recMayMinString(){
+    $sustit = str_replace("body", "black", "body and BODY");
+    echo $sustit;
+}
+recMayMinString();
+echo "<br>";
+
+#función reemplazar texto sin considerar mayus/min
+function susString(){
+$sustituir = str_ireplace("body", "black", "body and BODY");
+echo $sustituir;
+}
+susString();
+echo "<br>";
+
+#función string repetida
+function repeatString(){
+echo str_repeat("Hi ", 10);
+}
+repeatString();
+echo "<br>";
+
+#función longitud de string 
+function lonString(){
+$str = '123456789'; #devuelve 9
+echo strlen($str);
+}
+lonString();
+echo "<br>";
+
+#funcion primera aparición de un texto
 function number(){
-    $upCase = "yes";
-    $upcase = "no";
+    $ejem = "";
+    echo $ejem;
 }
 number();
 echo "<br>";
 
-#string
-echo "hello";
+#función en mayúscula
+function mayusString(){
+    $maSt = "capitalize this text";
+    $maSt = strtoupper($maSt);
+    echo $maSt;
+}
+mayusString();
 echo "<br>";
 
-#string
-echo "hello";
+#función en minúscula
+function minusString(){
+    $miSt = "lowerCASE tHIs TEXT";
+    $miSt = strtolower($miSt);
+    echo $miSt;
+}
+minusString();
 echo "<br>";
 
-#string
-echo "hello";
-echo "<br>";
-
-#string
-echo "hello";
-echo "<br>";
-
-#string
-echo "hello";
-echo "<br>";
-
-#string
-echo "hello";
-echo "<br>";
-
-#string
-echo "hello";
+#funcion para obtener una subcadena de texto, por posición
+function substring(){
+$rest = substr("abcdef", -3,-1);
+echo $rest;
+}
+substring();
 
 ?>
