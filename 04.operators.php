35077cbe9f107911988a921d6dcdc4061e13ffb0
Elirv <elizabeth.ramos101@hotmail.com>
2022-09-05 08:32:22 +0100

    all

--- 04.operators.php
+++ 04.operators.php
@@ -29,7 +29,9 @@ var_dump(2 &&4 <=3);
 echo"<br>";
 var_dump(0||5>1);
 echo"<br>";
-// var_dump(2!3);
-// echo"<br>";
+var_dump(5 Xor 1); #exclusivo si uno u otro lo comple, pero no los dos
+echo"<br>";
+var_dump(!3);
+echo"<br>";
 
 ?>
